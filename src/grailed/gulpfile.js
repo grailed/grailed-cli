cbb5d34ed6789c2da67e01e7c22f5197b2a664f2
David Tsuji <david@flodge.com>
2015-01-30 06:16:21 +1000

    fixes a bad ref to grailed

--- src/grailed/gulpfile.js
+++ src/grailed/gulpfile.js
@@ -1,5 +1,5 @@
 var env = require( './bin/grailed-env' ),
-	tasks = require( env.PATH_GRAILED + '/src/system/tasks' );
+	tasks = require( env.PATH_GRAILED + '/src/tasks' )( require( 'gulp' ) );
 
 [ 'build', 'default', 'minify', 'run' ].forEach( function ( _task ) {
 	gulp.task( _task, tasks[ _task ].concat( require( './tasks/' + _task ) ) );
